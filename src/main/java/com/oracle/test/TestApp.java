f1a7fa9b2f0b31229bd589a5f47700568816b285
rhl <rhl@qq.com>
2019-05-07 11:14:12 +0800

    开发了TestApp类

---
+++ src/main/java/com/oracle/test/TestApp.java
@@ -0,0 +1,11 @@
+package com.oracle.test;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Git");
+	}
+
+}
